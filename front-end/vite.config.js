506cc9ee68fc19ebc5b4cd040ce57aceaf40f645
Javi Calonge <javicalongem@gmail.com>
2024-11-19 09:04:06 +0100

    Cambiar port a 8000

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -12,12 +12,12 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000", // Backend en localhost:3000
+        target: "http://localhost:8000", // Backend en localhost:3000
         changeOrigin: true,
         secure: false,
       },
       "/images": {
-        target: "http://localhost:3000", // Servir imágenes desde el backend
+        target: "http://localhost:8000", // Servir imágenes desde el backend
         changeOrigin: true,
         secure: false,
       },
