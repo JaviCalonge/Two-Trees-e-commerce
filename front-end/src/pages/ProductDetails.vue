8b9300285b8eade66eb35c9a55d7aa0709354e7f
Javi Calonge <javicalongem@gmail.com>
2024-11-20 14:55:25 +0100

    npm run build

--- front-end/src/pages/ProductDetails.vue
+++ front-end/src/pages/ProductDetails.vue
@@ -94,7 +94,7 @@ async function signIn() {
   const auth = getAuth();
   const actionCodeSettings = {
     url: "https://two-trees-e-commerce.onrender.com/products",
-    //url: `https://two-trees-e-commerce.onrender.com/product/${route.params.id}`,
+    ////url: `https://two-trees-e-commerce.onrender.com/product/${route.params.id}`,
     handleCodeInApp: true,
   };
   try {
